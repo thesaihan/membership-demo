e1ed436b15ff10d50538c1a688b87b7ba66eb7e2
Sai Han <thesaihan@gmail.com>
2020-05-25 14:44:02 +0630

    Add edit and delete buttons

--- js/members.js
+++ js/members.js
@@ -39,7 +39,10 @@ function getRow(mem) {
   row.appendChild(col3);
 
   let col4 = document.createElement("td");
-  // col4.innerHTML = mem.team;
+  col4.innerHTML = `
+  <button class="btn btn-sm mx-3 btn-danger float-right"><i class="fa fa-trash"></i></button>
+  <button class="btn btn-sm mx-3 btn-outline-primary float-right"><i class="fa fa-pencil"></i></button>
+  `;
   row.appendChild(col4);
 
   return row;
